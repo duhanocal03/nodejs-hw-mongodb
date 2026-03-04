da66bbad9cdf3041f7b1ad0f1fa8f8ed15af3f08
Duhan Öçal <duhann.ocal@gmail.com>
2026-03-05 00:01:05 +0300

    feat: add modular OpenAPI docs for contacts endpoints

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const pinoHttp = require('pino-http');
-
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/openapi.yaml');
 const contactsRouter = require('./routers/contacts');
 const { errorHandler } = require('./middlewares/errorHandler');
 const notFoundHandler = require('./middlewares/notFoundHandler');
@@ -15,7 +17,7 @@ function setupServer() {
   app.use(express.json());
   app.use(pinoHttp());
   app.use(cookieParser());
-
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
 
